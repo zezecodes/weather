1584bea743ac01454e0c600804e5b6797295a9d2
Aaron Djangmah <96618832+zezecodes@users.noreply.github.com>
2023-11-23 15:00:21 +0000

    development

--- lib/features/homepage/presentation/bloc/homepage_state.dart
+++ lib/features/homepage/presentation/bloc/homepage_state.dart
@@ -1,9 +1,16 @@
 part of 'homepage_bloc.dart';
 
 abstract class HomepageState extends Equatable {
-  const HomepageState();  
+  const HomepageState();
 
   @override
   List<Object> get props => [];
 }
+
 class HomepageInitial extends HomepageState {}
+
+class HomepageLoading extends HomepageState {}
+
+class HomepageFailure extends HomepageState {}
+
+class HomepageSuccess extends HomepageState {}
